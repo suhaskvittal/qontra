09db6daa86aad70d4f209cd27306c94a801fc4bb
Suhas Vittal <suhaskvittal@gmail.com>
2024-01-22 18:11:16 -0700

    deleted more obsolete files

--- scripts/probius/rsc_memory_base.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ "${TRACE}" == "yes" ]; then
-    ./scripts/probius/rsc_memory_tracer_base.sh
-fi
-
-if [ "${DECODE}" == "yes" ]; then
-    ./scripts/probius/rsc_memory_decode_base.sh
-fi
