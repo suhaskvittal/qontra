a7eab68354fd5cbd4548dcc7524ad4ff9bab8e3e
Suhas Vittal <suhaskvittal@gmail.com>
2024-03-11 22:40:32 -0700

    before debugging decoders

--- scripts/protean/make_arch.py
+++ scripts/protean/make_arch.py
@@ -19,13 +19,14 @@ jid_blk = 'jid.ral.' if '-use-jid' in argv else ''
 verbose = '-v' if '-v' in argv else ''
 
 version_list = [
-    ('1', 'ral.rlb.rcr', 4, ''),
+#   ('1', f'{jid_blk}.rlb.rcr', 4, ''),
 #   ('2.1', 'fla.ral.rlb.rcr', 4, ''),
-    ('2.2', f'{jid_blk}fla.ral.{jid_blk}rlb.rcr', 4, '-flag-jid'),
+    ('2.2', f'{jid_blk}fla.ral.(prx.ral)+rlb.rcr', 4, ''),
+    ('2.3', f'{jid_blk}fla.ral.(prx.ral)+rlb.rcr', 3, ''),
 #   ('3.1', 'fla.ral(con.ral.prx.ral)+rlb.rcr', 4, ''),
-    ('3.2', f'{jid_blk}fla.ral.{jid_blk}(con.ral.prx.ral)+rlb.rcr', 4, '-flag-jid'),
+#   ('3.2', f'{jid_blk}fla.ral.{jid_blk}(con.ral.prx.ral)+rlb.rcr', 4, '-flag-jid'),
 #   ('4.1', 'fla.ral(con.ral.prx.ral)+rlb.rcr', 3, ''),
-    ('4.2', f'{jid_blk}fla.ral.{jid_blk}(con.ral.prx.ral)+rlb.rcr', 3, '-flag-jid'),
+#   ('4.2', f'{jid_blk}fla.ral.{jid_blk}(con.ral.prx.ral)+rlb.rcr', 3, '-flag-jid'),
 ]
 color_opt = '-color-checks' if '-color-checks' in argv else ''
 
