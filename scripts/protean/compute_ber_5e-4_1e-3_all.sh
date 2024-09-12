c9d3a88e2cd5cf28071d7cdd1eb1004a74ca5d50
Suhas Vittal <suhaskvittal@gatech.edu>
2024-09-12 10:59:17 -0400

    new script for executing jobs on aegeon

---
+++ scripts/protean/compute_ber_5e-4_1e-3_all.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+./scripts/protean/compute_ber_5e-4_1e-3_noslurm.sh hycc/4_6/216_40_8_8 restriction -mz
+./scripts/protean/compute_ber_5e-4_1e-3_noslurm.sh hycc/4_6/216_40_8_8 restriction -mx
